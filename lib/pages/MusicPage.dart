976ab2585cb13bbf305c8f669419ff0a80b38a7d
Scorpion645 <scorpion645@gmail.com>
2022-12-17 11:31:16 -0500

    complete

--- lib/pages/MusicPage.dart
+++ lib/pages/MusicPage.dart
@@ -28,25 +28,152 @@ class MusicPage extends StatelessWidget {
           child: Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            child: Container(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 45, horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Icon(
-                        CupertinoIcons.back,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                    )
-                  ],
+            child: Column(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 45, horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            CupertinoIcons.back,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.more_vert,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+                Spacer(),
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Imagine Dragon',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                        color: Colors.white.withOpacity(0.9))),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text('Singer Name',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white.withOpacity(0.8))),
+                              ],
+                            ),
+                            Icon(
+                              Icons.favorite,
+                              size: 35,
+                              color: Colors.redAccent,
+                            )
+                          ],
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Slider(
+                            min: 0,
+                            max: 100,
+                            onChanged: (double value) {},
+                            value: 40,
+                            activeColor: Colors.white,
+                            inactiveColor: Colors.white54,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 25),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('02:10',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                        color: Colors.white.withOpacity(0.6))),
+                                Text('04:30',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16,
+                                        color: Colors.white.withOpacity(0.6))),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.list,
+                            color: Colors.white,
+                            size: 32,
+                          ),
+                          Icon(
+                            CupertinoIcons.backward_end_fill,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            height: 55,
+                            width: 55,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                            
+                            ),
+                            child: Icon(
+                            Icons.play_arrow,
+                            color: Color(0xFF31314F),
+                            size: 45,
+                          ),
+                          ),
+                          Icon(
+                            CupertinoIcons.forward_end_fill,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          Icon(
+                            Icons.download,
+                            color: Colors.white,
+                            size: 32,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
